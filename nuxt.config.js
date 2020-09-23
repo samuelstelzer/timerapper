829ad0a164ede508a072ab9dcc6657be54e3b368
samuelstelzer <samuelstelzer@gmx.de>
2020-09-23 18:34:34 +0200

    more finishing touches on config

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,13 +28,13 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    "bootstrap-vue/nuxt",
-    "@nuxtjs/apollo"
+    "bootstrap-vue/nuxt"
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 
+  // Apollo/GraphQL Configuration (https://github.com/nuxt-community/apollo-module#setup)
   apollo: {
     clientConfigs: {
       default: {
